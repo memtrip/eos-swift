ad6d5106dc35b091674cf13346014f5ce33316be
Samuel Kirton <samkirton@me.com>
2018-10-03 17:15:26 +0100

    Initial Commit

---
+++ eos-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eos-swift
+//
+//  Created by Samuel Kirton on 03/10/2018.
+//  Copyright © 2018 memtrip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ eos-swiftTests/eos_swiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  eos_swiftTests.swift
+//  eos-swiftTests
+//
+//  Created by Samuel Kirton on 03/10/2018.
+//  Copyright © 2018 memtrip. All rights reserved.
+//
+
+import XCTest
+@testable import eos_swift
+
+class eos_swiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
